aa8ba1f7325289ade4e4bf804682c6166d9dcb3c
CRGRE <petr30032001@gmail.com>
2023-10-10 01:27:44 +0300

    add REDIRECT_TARGET

--- install/root/3-firewall.sh
+++ install/root/3-firewall.sh
@@ -6,17 +6,14 @@ firewall-cmd --permanent --new-zone=vpnserver
 
 firewall-cmd --permanent --zone=vpnserver --set-target=DROP
 
-firewall-cmd --permanent --zone=vpnserver --add-port=65122/tcp
 firewall-cmd --permanent --zone=vpnserver --add-port=22/tcp
 firewall-cmd --permanent --zone=vpnserver --add-port=80/tcp
-firewall-cmd --permanent --zone=vpnserver --add-port=65432/tcp
 firewall-cmd --permanent --zone=vpnserver --add-port=443/tcp
 firewall-cmd --permanent --zone=vpnserver --add-port=443/udp
-firewall-cmd --permanent --zone=vpnserver --add-port=65433/tcp
-firewall-cmd --permanent --zone=vpnserver --add-port=65434/tcp
+firewall-cmd --permanent --zone=vpnserver --add-port=65432/tcp
 
 firewall-cmd --permanent --zone=vpnserver --add-masquerade
-firewall-cmd --permanent --zone=vpnserver --add-forward-port=port=22:proto=tcp:toport=65022
+# firewall-cmd --permanent --zone=vpnserver --add-forward-port=port=22:proto=tcp:toport=65022
 firewall-cmd --permanent --zone=vpnserver --add-forward-port=port=80:proto=tcp:toport=65080
 firewall-cmd --permanent --zone=vpnserver --add-forward-port=port=443:proto=tcp:toport=65443
 firewall-cmd --permanent --zone=vpnserver --add-forward-port=port=443:proto=udp:toport=65443
@@ -25,3 +22,5 @@ firewall-cmd --reload
 firewall-cmd --set-default-zone=vpnserver
 
 firewall-cmd --info-zone=vpnserver
+
+# firewall-cmd --permanent --delete-zone=vpnserver
