d109a9841a9abd41a690ad9e6b6934e5410ff604
CRGRE <petr30032001@gmail.com>
2023-10-10 04:13:29 +0300

    added certbot hooks

--- install/root/5-certbot.sh
+++ install/root/5-certbot.sh
@@ -11,9 +11,9 @@ certbot certonly -d petchup.ddns.net --standalone \
     --http-01-port 65080 \
     --server https://acme-staging-v02.api.letsencrypt.org/directory
 
+PODMAN_USER="vpn"
 if id "$1" >/dev/null 2>&1
     then
-        PODMAN_USER="vpn"
         CERTBOT_HOOKS_PATH="$(eval realpath ~$PODMAN_USER/vps-vpn/certbot)"
         certbot renew --pre-hook $CERTBOT_HOOKS_PATH/pre-hook.sh --post-hook $CERTBOT_HOOKS_PATH/post-hook.sh
 fi
